b45b43f04507c94520c3530297b4f57615e4e355
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-16 15:56:26 +0600

    route add

--- react-side-bar/src/App.js
+++ react-side-bar/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from "./Pages/Dashboard";
+import Users from "./Pages/Users";
+import Anaytics from "./Pages/Anaytics";
+import FileManager from "./Pages/FileManager";
+import Messages from "./Pages/Messages";
+
+import Order from "./Pages/Order";
+import Saved from "./Pages/Saved";
+import Setting from "./Pages/Setting";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Route>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/users' element={<Users/>}/>
+        <Route path='/analytics' element={<Anaytics/>}/>
+        <Route path='/filemanager' element={<FileManager/>}/>
+        <Route path='/messages' element={<Messages/>}/>
+
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/saved' element={<Saved/>}/>
+        <Route path='/setting' element={<Setting/>}/>
+        
+
+
+
+      </Routes>
+    </Route>
+    </>
   );
 }
 
